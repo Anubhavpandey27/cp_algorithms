783a1ecb48d774cd4ad791943a9a161f174edb0d
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2024-02-03 16:41:50 +0530

    abcd

---
+++ number_theory/binary_exponentiation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#define ll long long
+ll binary_exp(ll a, ll b){
+    if(b==0){
+        return 1;
+    }
+    ll ex=binary_exp(a,b/2);
+    if((b&1)==1){
+        
+        return ex*ex*a;
+    }
+    else{
+        return ex*ex;
+
+    }
+}
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(NULL);
+
+cout<<binary_exp(2,3);
+
+}
